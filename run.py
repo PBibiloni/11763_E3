1ecdd137ea848bf6edfdd07b829316097e7e35c9
pedro <pedro@bibiloni.es>
2020-03-11 17:07:24 +0100

    Update initial code

--- run.py
+++ run.py
@@ -1,36 +1,10 @@
 import pydicom
 import numpy as np
-from matplotlib import pyplot as plt, animation
-from scipy import ndimage
+from matplotlib import pyplot as plt
 
 
 def load_dcm(filename):
     return pydicom.dcmread(f'data/{filename}')
-
-
-def rotate_YZ_v1(image, angle_in_degrees):
-    return ndimage.rotate(image, angle_in_degrees, axes=(1, 2), reshape=False)
-
-
-def rotate_YZ_v2(image, angle_in_degrees, background_padding_value):
-    y_grid, z_grid = np.meshgrid(np.linspace(0, image.shape[1], num=image.shape[1]),
-                                 np.linspace(0, image.shape[2], num=image.shape[2]))
-
-    rotated_y_grid = np.sin(angle_in_degrees) * (y_grid - image.shape[1] / 2) + np.cos(angle_in_degrees) * (z_grid - image.shape[2] / 2)
-    rotated_y_grid = np.round(rotated_y_grid + image.shape[1] / 2)
-    out_of_bounds_y = np.logical_or(rotated_y_grid < 0, rotated_y_grid >= image.shape[1])
-    rotated_y_grid = np.minimum(np.maximum(rotated_y_grid, 0), image.shape[1] - 1)
-
-    rotated_z_grid = np.cos(angle_in_degrees) * (y_grid - image.shape[1] / 2) - np.sin(angle_in_degrees) * (z_grid - image.shape[2] / 2)
-    rotated_z_grid = np.round(rotated_z_grid + image.shape[2] / 2)
-    out_of_bounds_z = np.logical_or(rotated_y_grid < 0, rotated_y_grid >= image.shape[2])
-    rotated_z_grid = np.minimum(np.maximum(rotated_z_grid, 0), image.shape[2] - 1)
-
-    rotated_image = image[:, rotated_y_grid.astype('int'), rotated_z_grid.astype('int')]
-    rotated_image[:, out_of_bounds_y] = background_padding_value
-    rotated_image[:, out_of_bounds_z] = background_padding_value
-
-    return rotated_image
 
 
 def main():
@@ -47,15 +21,6 @@ def main():
     plt.imshow(plano_medio_sagital, cmap=plt.cm.get_cmap('bone'), aspect=pixel_len_mm[0]/pixel_len_mm[1])
     plt.subplot(1, 2, 2)
     plt.imshow(plano_medio_coronal, cmap=plt.cm.get_cmap('bone'), aspect=pixel_len_mm[0]/pixel_len_mm[2])
-    plt.show()
-
-    # Proyecciones MIP / AIP coronales
-    coronal_MIP = np.max(img, axis=1)
-    coronal_AIP = np.mean(img, axis=1)
-    plt.subplot(1, 2, 1)
-    plt.imshow(coronal_MIP, cmap=plt.cm.get_cmap('bone'), aspect=pixel_len_mm[0]/pixel_len_mm[2])
-    plt.subplot(1, 2, 2)
-    plt.imshow(coronal_AIP, cmap=plt.cm.get_cmap('bone'), aspect=pixel_len_mm[0]/pixel_len_mm[2])
     plt.show()
 
 
